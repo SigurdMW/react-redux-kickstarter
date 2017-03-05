5acd4b8487943b2f6da5baa7c7c6bdf05bc379a1
SigurdMW <sigurdmwahl@gmail.com>
2017-03-05 14:38:50 +0100

    setup boilerplate redux implementation

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as actions from '../actions/actions'
+import Main from './Main'
+
+function mapStateToProps(state){
+	return {
+		tasks: state.tasks
+	}
+}
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators(actions, dispatch)
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Main)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+import './index.css'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// components
+import App from './components/App'
+import Home from './components/Home'
+
+const appRoutes = (
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={App}>
+				<IndexRoute component={Home} />
+			</Route>
+		</Router>
+	</Provider>
+)
+
+render(appRoutes, document.getElementById('root'))
